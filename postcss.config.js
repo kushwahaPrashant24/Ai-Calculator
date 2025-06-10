f91b04e8b5ab8fc9610dda37f4b6025aab983224
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2025-06-10 20:09:00 +0530

    feat: initialize Vite + React project with TypeScript and Tailwind CSS
    - Added package.json with dependencies for React, TypeScript, Vite, and Tailwind CSS.
    - Created postcss.config.js for Tailwind CSS integration.
    - Added SVG assets for Vite and React logos.
    - Implemented basic styling in App.css and index.css using Tailwind CSS.
    - Developed main application structure in App.tsx and main.tsx.
    - Configured Tailwind CSS in tailwind.config.ts.
    - Set up TypeScript configuration files for app and node.
    - Configured Vite with React plugin in vite.config.ts.

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {
+      tailwindcss: {},  // Config goes here now
+    },
+    autoprefixer: {},
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold text-purple-600">Hello Tailwind + TypeScript!</h1>
+    </>
+  )
+}
+
+export default App
